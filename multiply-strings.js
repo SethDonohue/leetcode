bf831b7842e78887d8b6c5ff46369c02a2ff04c0
Seth Donohue <sdonohue@paciolan.com>
2018-09-17 20:31:06 -0700

    working on padder

--- multiply-strings.js
+++ multiply-strings.js
@@ -1,12 +1,45 @@
 var multiply = function(num1, num2) {
 	let result = '0';
+	let newNumOne = num1;
+	let newNumTwo = num2;
 
 	if(num1 == '0') return num2;
 	if(num2 == '0') return num1;
 	// Long hand Multiplication
-	// Traverse shorter string, multiply each char by all the other chars in the longer string, sum the  results together
+	// pad shorter string with zeros
+	const padWithZeros = (shorterString, longerString) => {
+		console.log('Got to Padder');
+		while(shorterString.length < longerString.length){
+			console.log('Shorter Before: ', shorterString);
+			shorterString = '0' + shorterString;
+			console.log('Shorter After: ', shorterString);
+			}
+		console.log(shorterString, longerString);
+		return { shorterString, longerString };
+	};
+
+	console.log(num1.length, num2.length);
+	if(newNumOne.length > newNumTwo.length){
+//		padWithZeros(newNumTwo, newNumOne).num1 = num1;
+//		padWithZeros(newNumTwo, newNumOne).num2 = num2;
+	} else if(newNumTwo.length > newNumOne.length) {
+//		padWithZeros(newNumOne, newNumTwo).num1 = num1;
+//		padWithZeros(newNumOne, newNumTwo).num2 = num2;
+			console.log(padWithZeros(num1, num2));
+	}
+
+	console.log('After pad: ', num1, num2);
+
+	//while(
+
+	// multiply each char by all the other chars in the longer string
+
+	// sum the results together
+	
 
 	return result;
 };
+
+multiply('10', '100000');
 
 module.exports = multiply;
--- multiply-strings.test.js
+++ multiply-strings.test.js
@@ -10,9 +10,18 @@ const multiply = require('./multiply-strings.js');
 
 describe('Multiply Function', () => {
 	it('Adds two basic string numbers together correctly without a carry over:', () => {
-		const stringOne = '1111';
+		const stringOne = '111';
 		const stringTwo = '1111';
 
 		expect(multiply(stringOne, stringTwo)).toBe('2222');
 	})
+
+	it('Adds 0 as first or second argument without a carry over:', () => {
+		const stringOne = '0';
+		const stringTwo = '1111';
+
+		expect(multiply(stringOne, stringTwo)).toBe('1111');
+		expect(multiply(stringTwo, stringOne)).toBe('1111');
+	})
+
 })
